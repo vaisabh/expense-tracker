6b48995f3dfd27feac411791360727ffe569910a
vaisabh <vaibhavm1996@gmail.com>
2021-08-16 15:44:40 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './style.css';
+import ExpenseItem from './components/ExpenseItem';
 
-export default function App() {
+function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
+
+export default App;
