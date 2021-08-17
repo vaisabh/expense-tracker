78f1f33cd81467194f2602607b2ef8f227ca1fe0
vaisabh <vaibhavm1996@gmail.com>
2021-08-17 19:47:45 +0530

    4 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -27,27 +27,8 @@ function App() {
 
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <h1>Let's get Started</h1>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
